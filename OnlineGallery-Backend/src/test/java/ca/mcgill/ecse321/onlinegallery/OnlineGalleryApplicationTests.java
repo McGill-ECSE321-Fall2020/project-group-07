6203d1358b5e610b57a0ef74ba159038550f5ef2
jhanmtl <jing.han@mail.mcgill.ca>
2020-10-01 07:28:40 -0400

    Spring built OnlineGallery-Backend

---
+++ OnlineGallery-Backend/src/test/java/ca/mcgill/ecse321/onlinegallery/OnlineGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.onlinegallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
