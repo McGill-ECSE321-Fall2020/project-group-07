83c43d9dd21df735ee6e482b2a7b91586d34d8bf
anthonydagher <anthonyharissidagher@gmail.com>
2020-10-22 21:19:16 -0400

    Updated ArtistService + Form

--- OnlineGallery-Backend/src/main/java/ca/mcgill/ecse321/onlinegallery/service/ArtistService.java
+++ OnlineGallery-Backend/src/main/java/ca/mcgill/ecse321/onlinegallery/service/ArtistService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import ca.mcgill.ecse321.onlinegallery.dao.ArtistRepository;
 import ca.mcgill.ecse321.onlinegallery.dao.GalleryRegistrationRepository;
 import ca.mcgill.ecse321.onlinegallery.model.Artist;
+import ca.mcgill.ecse321.onlinegallery.model.ArtistForm;
+import ca.mcgill.ecse321.onlinegallery.model.GalleryRegistration;
 
 @Service
 public class ArtistService {
@@ -19,7 +21,27 @@ public class ArtistService {
 	GalleryRegistrationRepository regRepo;
 	
 	@Transactional
-	public Artist createArtist(Long artistId) {
+	public Artist createArtist(ArtistForm form) {
 		
+		String username = form.getUserName();
+		
+		if (!regRepo.existsByUserName(username)) {
+			
+			return null;
+		}
+		
+		GalleryRegistration reg = regRepo.findGalleryRegisrationByUserName(username);
+		if (reg.getArtist()==null) {
+			
+			reg.setArtist(new Artist());;
+		}
+		
+		Artist artist = regRepo.findGalleryRegisrationByUserName(username).getArtist();
+		
+		artist.setBankInfo(form.getBankInfo());
+		
+		artistRepo.save(artist);
+		
+		return artist;
 	}
 }
