862b75e175d13f9f6ce2bd02554d301d6d2e5f86
anthonydagher <anthonyharissidagher@gmail.com>
2020-10-25 17:09:05 -0400

    Update TestArtistServiceDelete.java

--- OnlineGallery-Backend/src/test/java/ca/mcgill/ecse321/onlinegallery/serviceAll/TestArtistServiceDelete.java
+++ OnlineGallery-Backend/src/test/java/ca/mcgill/ecse321/onlinegallery/serviceAll/TestArtistServiceDelete.java
@@ -1,5 +1,10 @@
 package ca.mcgill.ecse321.onlinegallery.serviceAll;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -7,7 +12,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import ca.mcgill.ecse321.onlinegallery.dao.ArtistRepository;
 import ca.mcgill.ecse321.onlinegallery.dao.GalleryRegistrationRepository;
+import ca.mcgill.ecse321.onlinegallery.dto.ArtistDto;
+import ca.mcgill.ecse321.onlinegallery.model.Artist;
 import ca.mcgill.ecse321.onlinegallery.service.ArtistService;
+import ca.mcgill.ecse321.onlinegallery.service.exception.ArtistException;
 
 @ExtendWith(MockitoExtension.class)
 public class TestArtistServiceDelete {
@@ -20,4 +28,27 @@ public class TestArtistServiceDelete {
 	
 	@InjectMocks
 	private ArtistService service;
+	
+	private static final String VALID_USERNAME = "ValidUserName";
+	private static final String INVALID_USERNAMENONEXIST = "nonexistentUser";
+	
+	private static final String VALID_BANKINFO = "moneymoneymoney is so funny";
+
+	//add mockup thing
+	
+	@Test
+	public void testValidArtistDelete() {
+		ArtistDto dto = new ArtistDto();
+		dto.setUsername(VALID_USERNAME);
+		dto.setBankInfo(VALID_BANKINFO);
+		
+		Artist a = null;
+		try {
+			a = service.deleteArtistByUserName(dto);
+		}catch(ArtistException e) {
+			fail();
+		}
+		assertNotNull(a);
+		assertEquals(a.getBankInfo(),VALID_BANKINFO);
+	}
 }
