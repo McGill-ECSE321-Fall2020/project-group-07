9d3565363f8c394ad08a64324bc709a42fe2d9c2
KeonOlszewski <olszkeon@gmail.com>
2020-10-27 14:19:41 -0400

    getAvailableArtworkDetail done

--- OnlineGallery-Backend/src/main/java/ca/mcgill/ecse321/onlinegallery/service/ArtworkService.java
+++ OnlineGallery-Backend/src/main/java/ca/mcgill/ecse321/onlinegallery/service/ArtworkService.java
@@ -1,70 +1,45 @@
-//package ca.mcgill.ecse321.onlinegallery.service;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import javax.transaction.Transactional;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import ca.mcgill.ecse321.onlinegallery.dao.ArtworkRepository;
-//import ca.mcgill.ecse321.onlinegallery.dao.GalleryRegistrationRepository;
-//import ca.mcgill.ecse321.onlinegallery.dao.OnlineGalleryRepository;
-//import ca.mcgill.ecse321.onlinegallery.dao.PhysicalGalleryRepository;
-//import ca.mcgill.ecse321.onlinegallery.model.Artist;
-//import ca.mcgill.ecse321.onlinegallery.model.Artwork;
+package ca.mcgill.ecse321.onlinegallery.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ca.mcgill.ecse321.onlinegallery.dao.ArtworkRepository;
+import ca.mcgill.ecse321.onlinegallery.dao.GalleryRegistrationRepository;
+import ca.mcgill.ecse321.onlinegallery.dao.OnlineGalleryRepository;
+import ca.mcgill.ecse321.onlinegallery.dao.PhysicalGalleryRepository;
+import ca.mcgill.ecse321.onlinegallery.model.Artist;
+import ca.mcgill.ecse321.onlinegallery.model.Artwork;
+import ca.mcgill.ecse321.onlinegallery.model.ArtworkStatus;
 //import ca.mcgill.ecse321.onlinegallery.model.ArtworkForm;
-//import ca.mcgill.ecse321.onlinegallery.model.GalleryRegistration;
-//import ca.mcgill.ecse321.onlinegallery.model.OnlineGallery;
-//import ca.mcgill.ecse321.onlinegallery.model.PhysicalGallery;
-//
-//@Service
-//public class ArtworkService {
-//
-//	@Autowired
-//	ArtworkRepository artRepo;
-//	
-//	@Autowired
-//	GalleryRegistrationRepository regRepo;
-//
-//	@Transactional
-//	public Artwork createArtworkInstance(ArtworkForm form) {
-//		
-//		String username=form.getUserName();
-//
-//		if (!regRepo.existsByUserName(username)) {
-//			return null;
-//			}
-//		
-//		GalleryRegistration reg=regRepo.findGalleryRegisrationByUserName(username);
-//		if (reg.getArtist()==null) {
-//			reg.setArtist(new Artist());;
-//			}
-//		
-//		Artist artist = regRepo.findGalleryRegisrationByUserName(username).getArtist();
-//		Artwork art = new Artwork();
-//		
-//		art.setName(form.getName());
-//		art.setDescription(form.getDescription());
-//		art.setPrice(form.getPrice());
-//		art.setStatus(form.getStatus());
-//		art.setOnSite(form.isOnSite());
-//		art.setNumViews(form.getNumViews());
-//		art.setDimension(form.getDimension());
-//		art.setWeight(form.getWeight());
-//		art.setShippingCost(form.getShippingcost());
-//		
-//		
-//		artist.getArtwork().add(art);
-//		art.setArtist(artist);
-//		
-//		artRepo.save(art);
-//		
-//		return art;
-//		
-//		
-//	}
-//
-//
-//}
+import ca.mcgill.ecse321.onlinegallery.model.GalleryRegistration;
+import ca.mcgill.ecse321.onlinegallery.model.OnlineGallery;
+import ca.mcgill.ecse321.onlinegallery.model.PhysicalGallery;
+import ca.mcgill.ecse321.onlinegallery.service.exception.ArtworkException;
+
+@Service
+public class ArtworkService {
+
+	@Autowired
+	ArtworkRepository artRepo;
+	
+	@Autowired
+	GalleryRegistrationRepository regRepo;
+
+	@Transactional
+	public Artwork getAvailableArtworkDetail(Long artworkId) throws ArtworkException{
+		if (!artRepo.existsByArtworkId(artworkId)) {
+			throw new  ArtworkException("No Available Artwork with artworkID ["+artworkId+"] exists");
+		} 
+
+		Artwork artwork = artRepo.findArtworkByArtworkId(artworkId);
+		if (artwork.getStatus() == ArtworkStatus.UNAVAILABLE) {
+			throw new  ArtworkException("No AvailableArtwork with artworkID ["+artworkId+"] exists");
+		} 
+		return artwork;
+	}
+}
