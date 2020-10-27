1497936d254cedc8b74da9566277d5ce2caabc8b
anthonydagher <anthonyharissidagher@gmail.com>
2020-10-27 15:30:49 -0400

    Changes to ArtistService & ArtworkService

--- OnlineGallery-Backend/src/main/java/ca/mcgill/ecse321/onlinegallery/service/ArtworkService.java
+++ OnlineGallery-Backend/src/main/java/ca/mcgill/ecse321/onlinegallery/service/ArtworkService.java
@@ -8,6 +8,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ca.mcgill.ecse321.onlinegallery.dao.ArtistRepository;
 import ca.mcgill.ecse321.onlinegallery.dao.ArtworkRepository;
 import ca.mcgill.ecse321.onlinegallery.dao.GalleryRegistrationRepository;
 import ca.mcgill.ecse321.onlinegallery.dto.ArtworkDto;
@@ -15,6 +16,7 @@ import ca.mcgill.ecse321.onlinegallery.model.Artist;
 import ca.mcgill.ecse321.onlinegallery.model.Artwork;
 import ca.mcgill.ecse321.onlinegallery.model.ArtworkStatus;
 import ca.mcgill.ecse321.onlinegallery.model.GalleryRegistration;
+import ca.mcgill.ecse321.onlinegallery.service.exception.ArtistException;
 import ca.mcgill.ecse321.onlinegallery.service.exception.ArtworkException;
 
 @Service
@@ -24,24 +26,30 @@ public class ArtworkService {
 	ArtworkRepository artworkRepo;
 	
 	@Autowired
+	ArtistRepository artistRepo;
+	
+	@Autowired
 	GalleryRegistrationRepository regRepo;
 	
 
 	@Transactional
-	public Artwork createArtwork(ArtworkDto dto) throws ArtworkException {
+	public Artwork createArtwork(Long artistId, ArtworkDto dto) throws ArtworkException, ArtistException {
 		
-		String username=dto.getUsername();
-				
+		if(!artistRepo.existsById(artistId)) {
+			throw new ArtistException("No artist with ID ["+artistId+"] exists");
+		}
+		
+		String username=dto.getUsername();		
 		if (!regRepo.existsByUserName(username)) {
 			throw new ArtworkException("No GalleryRegistration with username ["+username+"] exists");
 		}
 		
 		GalleryRegistration reg=regRepo.findGalleryRegisrationByUserName(username);
 		if (reg.getArtist()==null) {
-			reg.setArtist(new Artist());;
+			throw new ArtworkException("No artist associated with username ["+username+"]");
 		}
 		
-		Artist artist = regRepo.findGalleryRegisrationByUserName(username).getArtist();
+		Artist artist = reg.getArtist();
 		Artwork art = new Artwork();
 		
 		art.setName(dto.getName());
@@ -62,23 +70,17 @@ public class ArtworkService {
 		return art;
 		
 	}
-<<<<<<< HEAD
-=======
 	
 	@Transactional
 	public Artwork getAvailableArtworkDetail(Long artworkId) throws ArtworkException{
-		if (!artRepo.existsByArtworkId(artworkId)) {
+		if (!artworkRepo.existsByArtworkId(artworkId)) {
 			throw new  ArtworkException("No Available Artwork with artworkID ["+artworkId+"] exists");
 		} 
 
-		Artwork artwork = artRepo.findArtworkByArtworkId(artworkId);
+		Artwork artwork = artworkRepo.findArtworkByArtworkId(artworkId);
 		if (artwork.getStatus() == ArtworkStatus.UNAVAILABLE) {
 			throw new  ArtworkException("No AvailableArtwork with artworkID ["+artworkId+"] exists");
 		} 
 		return artwork;
 	}
-	
-
-
->>>>>>> origin/keon-rest
 }
