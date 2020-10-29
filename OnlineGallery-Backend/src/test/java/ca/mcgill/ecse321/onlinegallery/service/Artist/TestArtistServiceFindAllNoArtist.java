5cd4006aab5458bbbf8c6f6557b4fb833c0de11b
anthonydagher <anthonyharissidagher@gmail.com>
2020-10-28 21:54:01 -0400

    Completed Artist tests - cover 100%

--- OnlineGallery-Backend/src/test/java/ca/mcgill/ecse321/onlinegallery/service/Artist/TestArtistServiceFindAllNoArtist.java
+++ OnlineGallery-Backend/src/test/java/ca/mcgill/ecse321/onlinegallery/service/Artist/TestArtistServiceFindAllNoArtist.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse321.onlinegallery.service.Artist;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.lenient;
 
@@ -17,8 +18,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ca.mcgill.ecse321.onlinegallery.dao.ArtistRepository;
 import ca.mcgill.ecse321.onlinegallery.model.Artist;
 import ca.mcgill.ecse321.onlinegallery.model.Artwork;
-import ca.mcgill.ecse321.onlinegallery.model.ArtworkStatus;
-import ca.mcgill.ecse321.onlinegallery.model.GalleryRegistration;
 import ca.mcgill.ecse321.onlinegallery.service.ArtistService;
 import ca.mcgill.ecse321.onlinegallery.service.exception.ArtistException;
 
@@ -31,25 +30,11 @@ public class TestArtistServiceFindAllNoArtist {
 	@InjectMocks
 	private ArtistService service;
 	
-	private static final Long ARTWORKTID = (long) 1;
-
-	private static final String VALID_USERNAME = "ValidUserName";
 	@BeforeEach
 	public void setMockOutput() {		
 		lenient().when(artistRepo.findAll()).thenAnswer((InvocationOnMock invocation) -> {
 			
 			List<Artwork> list = new ArrayList<Artwork>();
-			GalleryRegistration reg = new GalleryRegistration();
-			reg.setUserName(VALID_USERNAME);
-			Artwork a = new Artwork();
-			Artist artist = new Artist();
-			a.setArtist(artist);
-			a.setArtworkId(ARTWORKTID);
-			artist.getArtwork().add(a);
-			a.setStatus(ArtworkStatus.UNAVAILABLE);
-			reg.setArtist(artist);
-			artist.setGalleryRegistration(reg);
-			list.add(a);
 			
 			return list;
 		});
