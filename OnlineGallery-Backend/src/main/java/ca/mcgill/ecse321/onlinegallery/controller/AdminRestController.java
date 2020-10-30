51aa523b41c2c2adff2915d4912bb45ceaeca1f4
Massimo Vadacchino <TazMass@hotmail.com>
2020-10-30 17:55:58 -0400

    Finished Customer and Admin REST constoller [doing postman tests nows for all three]

---
+++ OnlineGallery-Backend/src/main/java/ca/mcgill/ecse321/onlinegallery/controller/AdminRestController.java
@@ -0,0 +1,30 @@
+package ca.mcgill.ecse321.onlinegallery.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.onlinegallery.model.GalleryAdmin;
+import ca.mcgill.ecse321.onlinegallery.service.AdminService;
+import ca.mcgill.ecse321.onlinegallery.service.exception.AdminException;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class AdminRestController {
+
+	@Autowired
+	AdminService service;
+	
+	@GetMapping(value = { "/getAdmin", "/getAdmin/" })
+	public ResponseEntity<?> getAdmin() throws AdminException {
+		try {
+			GalleryAdmin admin = service.getAdmin();
+			return new ResponseEntity<>(admin, HttpStatus.OK);			
+		} catch(AdminException e) {
+			return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+		}
+	}
+}
--- OnlineGallery-Backend/src/main/java/ca/mcgill/ecse321/onlinegallery/controller/PurchaseRestController.java
+++ OnlineGallery-Backend/src/main/java/ca/mcgill/ecse321/onlinegallery/controller/PurchaseRestController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +26,7 @@ public class PurchaseRestController {
 	@Autowired
 	PurchaseService service;
 	
-	@GetMapping(value = { "/createPurchase", "/createPurchase/" })
+	@PostMapping(value = { "/createPurchase", "/createPurchase/" })
 	public ResponseEntity<?> createPurchase(@RequestBody PurchaseDto dto) throws PurchaseException {
 		try {
 			Purchase purchase = service.createPurchase(dto);
@@ -57,7 +60,7 @@ public class PurchaseRestController {
 		}
 	}
 	
-	@GetMapping(value = { "/updatePurchase", "/updatePurchase/" })
+	@PutMapping(value = { "/updatePurchase", "/updatePurchase/" })
 	public ResponseEntity<?> updatePurchaseShipment(@RequestBody PurchaseDto dto) throws PurchaseException {
 		try {
 			Purchase purchase = service.updatePurchaseShipment(dto);
@@ -68,7 +71,7 @@ public class PurchaseRestController {
 		}
 	}
 	
-	@GetMapping(value = { "/deletePurchase", "/deletePurchase/" })
+	@DeleteMapping(value = { "/deletePurchase", "/deletePurchase/" })
 	public ResponseEntity<?> deletePurchase(@RequestBody PurchaseDto dto) throws PurchaseException {
 		try {
 			Purchase purchase = service.deletePurchase(dto);
