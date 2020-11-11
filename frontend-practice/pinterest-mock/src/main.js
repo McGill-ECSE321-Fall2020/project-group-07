c11233758f74cb90a26fb4a0305cd5c1f966e4f1
anthonydagher <anthonyharissidagher@gmail.com>
2020-11-10 19:27:10 -0500

    Artist artwork page set-up

--- frontend-practice/pinterest-mock/src/main.js
+++ frontend-practice/pinterest-mock/src/main.js
@@ -6,10 +6,11 @@ import VueRouter from "vue-router";
 import Home from "@/views/Home";
 import About from "@/views/About"
 import DiscoverPage from "@/views/DiscoverPage";
-import AllArtists from "@/views/AllArtists";
+//import AllArtists from "@/views/AllArtists";
 import CustomerLogin from "@/views/CustomerLogin";
 import ArtistLogin from "@/views/ArtistLogin";
 import Register from "@/views/Register";
+import ArtistPage from "@/views/ArtistPage";
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
@@ -19,7 +20,7 @@ const router = new VueRouter({
     {path:'/home',component:Home},
     {path:"/about",component: About},
     {path:'/discover',component:DiscoverPage},
-    {path:'/all-artists',component:AllArtists},
+    {path:'/all-artists',component:ArtistPage},
     {path:"/customer-login", component: CustomerLogin},
     {path:"/artist-login", component:ArtistLogin},
     {path:"/register", component: Register},
