ebd8339a157e8ac5fb2914a542a696f60c8cf99c
KeonOlszewski <olszkeon@gmail.com>
2020-11-13 11:59:50 -0500

    added backend request

--- OnlineGallery-Frontend(Keon)/loginmock/src/components/login.vue
+++ OnlineGallery-Frontend(Keon)/loginmock/src/components/login.vue
@@ -1,96 +1,78 @@
 <template>
-<v-container>
-    <v-card
-    class="mx-auto"
-    style="max-width: 500px;"
-  >
+  <v-container>
+    <v-card class="mx-auto" style="max-width: 500px">
       <v-toolbar>
-      <v-card-title class="title font-weight-regular">
-        Login
-      </v-card-title>
+        <v-card-title class="title font-weight-regular"> Login </v-card-title>
       </v-toolbar>
- <v-form
-    ref="form"
-    v-model="form"
-    class="pa-4 pt-6"
-  >
-    <v-text-field
-      v-model="username"
-      :counter="10"
-      :rules="username"
-      label="Username"
-      required
-    ></v-text-field>
-    
 
+      <v-form class="pa-4 pt-6">
+        <v-text-field v-model="username" label="Username" required />
 
-    <v-text-field
-      v-model="password"
-       :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-       :rules="passwordRules"
-       :type="show1 ? 'text' : 'password'"
-       name="input-10-1"
-       label="Password"
-       counter
-       @click:append="show1 = !show1"
-    ></v-text-field>
- </v-form>
+        <v-text-field
+          v-model="password"
+          :type="'password'"
+          name="input-10-1"
+          label="Password"
+        />
+      </v-form>
 
-    <v-divider></v-divider>
-    <v-card-actions>
-    <v-btn
-      class="mr-4"
-      @click="Login"
-    >
-      Login
-    </v-btn>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn class="mr-4" @click="Login"> Login </v-btn>
       </v-card-actions>
+      <v-card-text class="text-center">{{ responseMsg }}</v-card-text>
     </v-card>
-</v-container>
+  </v-container>
 </template>
 
 
 <script>
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
 export default {
-    name: 'login',
+  name: "login",
 
-    data: () => ({
-      username: '',
-      usernameRules: [
-        v => !!v || 'Username is required',
-      ],
-      
-      password: '',
-      passwordRules: [
-        v => !!v || 'Password is required',
-      ],
-      
-    }),
+  data: () => ({
+    username: "",
+    password: "",
+    responseMsg: "",
+  }),
 
-    methods: {
-      Login(){
-        var loginInfo={
-          username: this.username,
-          password: this.password,
-         
-        }
-        console.log(loginInfo)
-      }
-    }
-    
-}
+  methods: {
+    Login() {
+      var loginInfo = {
+        username: this.username,
+        password: this.password,
+      };
+      console.log(loginInfo);
+      axios
+        .post(
+          "https://onlinegallery-backend-g7.herokuapp.com/customerLogin", 
+          loginInfo //not sure if its the right request
+        ) 
+        .then((res) => {
+          this.responseMsg = this.username + " Logged in!";
+          console.log(res);
+        })
+        .catch((error) => {
+          this.responseMsg = error.response.data;
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-.active{
+.active {
   cursor: pointer;
 }
-.title{
+.title {
   font-family: Roboto;
   text-align: center;
 }
-.card{
+.card {
   border: 1px solid black !important;
 }
 </style>
