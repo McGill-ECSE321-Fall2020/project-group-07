6f3702331479cf8c81508531647e3d3f4222ec22
anthonydagher <anthonyharissidagher@gmail.com>
2020-11-13 10:03:30 -0500

    Discover Artist started

--- frontend-practice/pinterest-mock/src/views/AllArtists.vue
+++ frontend-practice/pinterest-mock/src/views/AllArtists.vue
@@ -1,7 +1,25 @@
 <template>
-    <div class="placeholder">
-      <div> all artists page</div>
-    </div>
+  <v-container fluid>
+      <div>
+      <HeaderBar/>
+          <div class="masonry-container">
+
+            <div class="refresh">
+              <v-btn text>
+                <v-icon class="black-link">
+                  mdi-refresh
+                </v-icon>
+              </v-btn>
+            </div>
+
+            <v-row dense no-gutters v-masonry origin-left="true" horizontal-order="true" item-selector=".item">
+              <v-col :sm="totalWidth" v-for="each in artworks" v-masonry-tile class="item" :key="each.id" >
+               <ImageTile :artid="each.id" :imgUrl="each.imgUrl" :artname="each.name" :artistname="each.username" :artdesc="each.description" :medium="each.medium" :dimension="each.dimension" :price="each.price" :height="each.height"/>
+              </v-col>
+            </v-row>
+          </div>
+       </div>
+  </v-container>
 </template>
 
 <script>
@@ -14,11 +32,20 @@ export default {
 }
 </script>
 
-<style>
-
-.placeholder{
-  padding-top: 300px;
-  text-align: center;
+<style scoped>
+.masonry-container {
+  width: 75%;
+  margin:auto;
+  margin-top: 100px;
 }
 
+.refresh{
+  text-align: center;
+  margin-bottom: 50px;
+}
+
+.black-link{
+  color:black !important;
+  text-decoration: none !important;
+}
 </style>
