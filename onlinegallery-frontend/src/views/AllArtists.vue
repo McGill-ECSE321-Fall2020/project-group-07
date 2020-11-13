6ecf358ca1b94bc2dd646472cfcc5c2faf19ee8a
jhanmtl <jing.han@mail.mcgill.ca>
2020-11-13 07:36:31 -0500

    copied practice frontend to official  frontend dir

---
+++ onlinegallery-frontend/src/views/AllArtists.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="placeholder">
+      <div> all artists page</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'artist-page',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+.placeholder{
+  padding-top: 300px;
+  text-align: center;
+}
+
+</style>
