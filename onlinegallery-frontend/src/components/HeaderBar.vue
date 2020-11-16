ce41ce26eb676000ff67eb0b2c4426f2a334f2b9
jhanmtl <jing.han@mail.mcgill.ca>
2020-11-15 22:10:25 -0500

    sprint3 done

--- onlinegallery-frontend/src/components/HeaderBar.vue
+++ onlinegallery-frontend/src/components/HeaderBar.vue
@@ -62,7 +62,7 @@
 
           <v-col :sm="1" class = "col facebook">
             <v-btn text x-small href="https://www.facebook.com/Completely-Secure-Gallery-107431924517668" target="_blank">
-              <v-icon color="#000000">
+              <v-icon color="#050505">
                 mdi-facebook
               </v-icon>
             </v-btn>
