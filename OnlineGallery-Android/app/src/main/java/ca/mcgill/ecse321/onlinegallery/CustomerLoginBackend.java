b12a1bd46bf2560b8efe9321a445cfbdd6e58e95
KeonOlszewski <olszkeon@gmail.com>
2020-11-25 17:10:58 -0500

    changed gradle build

---
+++ OnlineGallery-Android/app/src/main/java/ca/mcgill/ecse321/onlinegallery/CustomerLoginBackend.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ecse321.onlinegallery;
+
+import java.util.List;
+
+import ca.mcgill.ecse321.onlinegallery.dto.GalleryRegistrationDto;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import io.reactivex.Observable;
+
+public interface CustomerLoginBackend {
+
+   @GET("/getCustomerByUsername/{username}")
+    Observable<CustomerDto> getCustomerByUsername(@Path("username") String username);
+}
