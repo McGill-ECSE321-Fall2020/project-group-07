52a8ebee3d95ee46db12061a35365aa94366953d
jhanmtl <jing.han@mail.mcgill.ca>
2020-11-26 10:56:26 -0500

    uploading not working

--- android-practice/retrofitrxjava/app/src/main/java/ca/mcgill/ecse321/retrofit_rxjava/UploadActivity.java
+++ android-practice/retrofitrxjava/app/src/main/java/ca/mcgill/ecse321/retrofit_rxjava/UploadActivity.java
@@ -1,17 +1,27 @@
 package ca.mcgill.ecse321.retrofit_rxjava;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
 import java.io.File;
 
 public class UploadActivity extends AppCompatActivity {
+    private static final int RESULT_LOAD_IMAGE = 1;
+    private static final int PERMISSION_REQUEST_CODE = 2;
 
     private static final String TAG = "UploadActivity";
     Bitmap img;
@@ -22,10 +32,12 @@ public class UploadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
 
+    }
 
-        img = BitmapFactory.decodeFile("/storage/emulated/0/DCIM/sunrise.jpg");
-//        Log.e(TAG, "onCreate: "+img.getWidth());
-//        Log.e(TAG, "onCreate: "+img.getHeight());
+    public void browse(View view){
+        Log.e(TAG, "browse: "+Environment.getExternalStorageDirectory());
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.getContentUri("/storage/emulated/0/Pictures"));
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
     }
 
 
@@ -35,5 +47,18 @@ public class UploadActivity extends AppCompatActivity {
 
     public void upload(View view){
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //called when image is selected from gallery
+
+        //make sure is called from gallery
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data!= null){
+            //get address of selected image
+            Uri selectedImage = data.getData();
+            //display it
+        }
     }
 }
