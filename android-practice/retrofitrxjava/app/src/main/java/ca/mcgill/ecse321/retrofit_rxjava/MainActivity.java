2f8da02e8f9679f9d48084f095873080e4b25f04
Massimo Vadacchino <TazMass@hotmail.com>
2020-11-27 14:23:16 -0500

    Minor changes to UX/UI design for purchase history

--- android-practice/retrofitrxjava/app/src/main/java/ca/mcgill/ecse321/retrofit_rxjava/MainActivity.java
+++ android-practice/retrofitrxjava/app/src/main/java/ca/mcgill/ecse321/retrofit_rxjava/MainActivity.java
@@ -1,32 +1,11 @@
 package ca.mcgill.ecse321.retrofit_rxjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import ca.mcgill.ecse321.retrofit_rxjava.dto.ArtworkDto;
-import ca.mcgill.ecse321.retrofit_rxjava.dto.AvailableNumDto;
-import io.reactivex.ObservableSource;
-import io.reactivex.Scheduler;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
-import io.reactivex.schedulers.Schedulers;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
-
-import io.reactivex.Observable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG="MainActivity";
